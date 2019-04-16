cb651d20796f17245ece1819de5d82b4c511799c
Mason <mason.a.traylor@gmail.com>
2019-04-15 18:26:33 -0600

    Remove improperly names StaticPage controller

--- test/controllers/static_page_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class StaticPageControllerTest < ActionDispatch::IntegrationTest
-  test "should get contact" do
-    get static_page_contact_url
-    assert_response :success
-  end
-
-end
